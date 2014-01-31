b38c3831b7ad01b0b9a5644a465ec463d06738f4
Ivaylo Petrov <ivajloip@gmail.com>
2014-01-31 17:55:12 +0100

    Adding simple odt parser - for now it just gets an odt file and extracts the rows from the first table

---
+++ odt_parser.py
@@ -0,0 +1,84 @@
+from lxml import etree
+import zipfile
+import re
+
+ODF_NAMESPACES = {
+  'anim': "urn:oasis:names:tc:opendocument:xmlns:animation:1.0",
+  'chart': "urn:oasis:names:tc:opendocument:xmlns:chart:1.0",
+  'config': "urn:oasis:names:tc:opendocument:xmlns:config:1.0",
+  'dc': "http://purl.org/dc/elements/1.1/",
+  'dom': "http://www.w3.org/2001/xml-events",
+  'dr3d': "urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0",
+  'draw': "urn:oasis:names:tc:opendocument:xmlns:drawing:1.0",
+  'fo': "urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0",
+  'form': "urn:oasis:names:tc:opendocument:xmlns:form:1.0",
+  'math': "http://www.w3.org/1998/Math/MathML",
+  'meta': "urn:oasis:names:tc:opendocument:xmlns:meta:1.0",
+  'number': "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0",
+  'of': "urn:oasis:names:tc:opendocument:xmlns:of:1.2",
+  'office': "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+  'ooo': "http://openoffice.org/2004/office",
+  'oooc': "http://openoffice.org/2004/calc",
+  'ooow': "http://openoffice.org/2004/writer",
+  'presentation': "urn:oasis:names:tc:opendocument:xmlns:presentation:1.0",
+  'rdfa': "http://docs.oasis-open.org/opendocument/meta/rdfa#",
+  'rpt': "http://openoffice.org/2005/report",
+  'script': "urn:oasis:names:tc:opendocument:xmlns:script:1.0",
+  'smil': "urn:oasis:names:tc:opendocument:xmlns:smil-compatible:1.0",
+  'style': "urn:oasis:names:tc:opendocument:xmlns:style:1.0",
+  'svg': "urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0",
+  'table': "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
+  'text': "urn:oasis:names:tc:opendocument:xmlns:text:1.0",
+  'xforms': "http://www.w3.org/2002/xforms",
+  'xlink': "http://www.w3.org/1999/xlink",
+  'xsd': "http://www.w3.org/2001/XMLSchema",
+  'xsi': "http://www.w3.org/2001/XMLSchema-instance",
+  'manifest': "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0",
+  'xml': 'http://www.w3.org/XML/1998/namespace'
+  } 
+
+#def has_cyrilic_letters(text):
+  #return re.match('^.*[а-яА-Я].*$', text) != None
+
+#def get_words(file_obj):
+  #tree = etree.parse(file_obj)
+  #root = tree.getroot()
+  #table = root.find('.//table:table', ODF_NAMESPACES)
+
+  #cells = table.findall('.//table:table-cell/', ODF_NAMESPACES)
+  #cells_text = [re.sub('  +', ' ', ' '.join(_.itertext())) for _ in cells]
+
+  #word_pairs = []
+  #index = 0
+
+  #while(index < len(cells)):
+    #next_index = index + 2
+    #word = cells_text[index]
+    #translation = cells_text[index + 1]
+
+    ## This is added as sometimes two consequtive cells belong to the translation
+    ## It is an ugly hack that should be avoided if possible
+    #while(next_index < len(cells) and has_cyrilic_letters(cells_text[next_index])):
+      #translation += cells_text[next_index]
+      #next_index += 1
+
+    #word_pairs.append((word, translation))
+    #index = next_index
+
+  #return word_pairs
+
+def get_words(file_obj):
+  tree = etree.parse(file_obj)
+  root = tree.getroot()
+  table = root.find('.//table:table', ODF_NAMESPACES)
+
+  rows = table.findall('.//table:table-row/', ODF_NAMESPACES)
+  rows_text = [re.sub('  +', ' ', ' '.join(_.itertext())) for _ in rows]
+
+  return zip(rows_text[::2], rows_text[1::2])
+
+def parse_odt(filepath):
+  with zipfile.ZipFile(filepath) as odt_file:
+    return get_words(odt_file.open('content.xml'))
+ 
+#print('\n'.join([str(_) for _ in parse_odt('/tmp/00000002.vocabulaire_3_A2.odt')]))
